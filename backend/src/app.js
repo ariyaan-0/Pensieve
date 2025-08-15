5fa09c38a6c93fc8724512520d3837959247abea
Md Rezaul Hossain <str.rezaul@gmail.com>
2025-08-15 18:12:54 +0600

    utils code

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//Task: Study CORS
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+//Configuration---
+app.use(express.json({limit: "16kb"})); //data limitation for json
+app.use(express.urlencoded({extended: true, limit: "16kb"})); //data from url
+app.use(express.static("public")); //if needed for file
+app.use(cookieParser());
+
+
+
+export {app};
---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err));
+    }
+}
+
+export {asyncHandler};
+
+//higher order function---
+//const asyncHandler = (func) => { async () = > {} } //we can eleminate the outer {}
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try{
+        await fn(req, res, next);
+    } catch (error){
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+*/
